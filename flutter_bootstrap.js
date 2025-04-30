7e109e7c9730b8208bcbcc23f8abde94379ac2a4
Jeffrey Brite <jbrite504@gmail.com>
2025-04-30 18:00:09 -0500

    Deploy updated Crucible Helper web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2843752272"
+    serviceWorkerVersion: "4146587472"
   }
 });
